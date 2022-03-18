e2f52a2137ddba184d75a819415eeeef3872ef61
Ali Rios <ali.rios@gmail.com>
2022-03-18 06:40:33 -0300

    🎉 Initialization project create files main, models, database

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from sqalchemy.orm import relationship
+
+from .database import Base
+
+
+class Note(Base):
+    __tablename__ = "notes"
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String, nullable=True, default="new")
